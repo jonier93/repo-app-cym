eabc6b09522646b6392e0913f3ac97b599b9f2b3
EC2 Default User <ec2-user@ip-172-31-42-152.ec2.internal>
2024-08-17 22:22:06 +0000

    register_user_in_database

--- database/db.py
+++ database/db.py
@@ -5,13 +5,28 @@ db_user = "jonier"
 db_password = "12345678"
 db_database = "db_users"
 
-try: 
-    pymysql.connect(
-        host=db_host,
-        user=db_user,
-        password=db_password,
-        database=db_database
-    )
-    print("Succesfull connection to database")
-except Exception as err:
-    print("Error connecting to database:", err)
+def connectionSQL(): 
+    try: 
+        connection = pymysql.connect(
+            host=db_host,
+            user=db_user,
+            password=db_password,
+            database=db_database
+        )
+        print("Succesfull connection to database")
+        return connection
+    except Exception as err:
+        print("Error connecting to database:", err)
+        return None
+
+def add_user(ident, name, lastname, birthday):
+    instruction = "INSERT INTO users VALUES ("+ident+", '"+name+"', '"+lastname+"', '"+birthday+"')"
+    connection = connectionSQL()
+    cursor = connection.cursor()
+    try:
+        cursor.execute(instruction)
+        connection.commit()
+        print("User added")
+    except:
+        print("Error when adding the user")
+    return True
--- server.py
+++ server.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+from database.db import *
 
 app = Flask(__name__)
 
@@ -10,7 +11,14 @@ def home_page():
 def register_page():
     return render_template("register.html")
 
+@app.route("/register_user", methods=["post"])
+def register_user():
+    data_user = request.form
+    ident, name, lastname, birthday = data_user["id"], data_user["name"], data_user["lastname"], data_user["birthday"]
+    add_user(ident, name, lastname, birthday)
+    return "<h1> User was added </h1>"
+
 if __name__ == "__main__":
     host = "0.0.0.0"
     port = "80"
-    app.run(host, port)
+    app.run(host, port, debug=True)   
